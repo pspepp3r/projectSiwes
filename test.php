1e36aa9305f2be8accd50c4695534b1d4be0f67a
pspepp3r <prosperpepple12@gmail.com>
2024-09-24 09:53:23 +0100

    added git commands to test.php

--- test.php
+++ test.php
@@ -1,3 +1,6 @@
 <?php
 // I'm still new to this stuff so, I created this file so I could test
-// whatever I'm not so sure of...
+// whatever I'm not so sure of... 
+// git remote add origin https://github.com/pspepp3r/projectSiwes.git
+// git branch -M main
+// git push -u origin main
